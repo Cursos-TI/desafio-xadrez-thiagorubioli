6203b14d25882bb8e75749310af7217b59ab1d95
Thiago Rubioli <thiagofmc@gmail.com>
2025-09-10 00:50:13 +0000

    Nível Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -8,10 +8,12 @@ int main () {
 
     for (int t = 0; t < 5; t++) {
         
-        // Mover a torre 5 casas para a direita
+        // Mover a torre 5 casas para a direita (imprime "Direita" 5 vezes)
         printf("Direita\n");
         
     }
+
+    printf(">>> TORRE moveu 5 casas para a direita <<<\n");
 
     printf("\nMovimentação do bispo:\n");
 
@@ -19,7 +21,7 @@ int main () {
     
     while (b < 5) {
 
-        // Mover o bispo 5 casas para a diagonal (cima/direita)
+        // Mover o bispo 5 casas para a diagonal (imprime "Cima/Direita" 5 vezes)
         printf("Cima\n");
         printf("Direita\n");
         
@@ -27,18 +29,39 @@ int main () {
         
     }
 
+    printf(">>> BISPO moveu 5 casas para a cima e 5 casas para a direita <<<\n");
+    
     printf("\nMovimentação da rainha:\n");
     
     int r = 0;
 
     do {
 
-        // Mover a rainha 8 casas para a esquerda
+        // Mover a rainha 8 casas para a esquerda (imprime "Esquerda" 8 vezes)
         printf("Esquerda\n");
 
         r++;
 
     } while (r < 8);
+
+    printf(">>> RAINHA moveu 8 casas para a esquerda <<<\n");
+    
+    printf("\nMovimentação do cavalo:\n");
+
+    int movimento_cavalo = 1; //Flag para controlar o movimento em "L"
+
+    while (movimento_cavalo--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            // Mover o cavalo 2 casas para baixo (imprime "Baixo" 2 vezes)
+            printf("Baixo\n");
+        }
+        // Mover o cavalo 1 casa para a esquerda (imprime "Esquerda" 1 vez)
+        printf("Esquerda\n"); // Imprime "Esquerda" 1 vez
+    }
+
+    printf(">>> CAVALO moveu 2 casas para baixo e 1 casa para a esquerda <<<\n\n");
 
     return 0;
 
